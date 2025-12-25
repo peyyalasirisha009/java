295082a5a1b7ec401cfbca40c8368a42c091a76d
Sirisha <peyyalasirisha009@gmail.com>
2025-12-25 22:44:45 +0530

    roll

---
+++ roll.java
@@ -0,0 +1,14 @@
+class Student {
+    int roll;
+    String name;
+
+    Student(){
+        roll = 0;
+        name = "Unknown";
+    }
+
+    Student(int r, String n){
+        roll = r;
+        name = n;
+    }
+}
